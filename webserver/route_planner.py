1a0431084eb0c4177d957c89224ca26b9bc8f379
AhmadGhaemi <ahmad.ghaemi@hotmail.com>
2022-05-16 22:23:28 +0200

    servers.key didn't have right path

--- webserver/route_planner.py
+++ webserver/route_planner.py
@@ -79,8 +79,8 @@ def route_planner():
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_3)
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations("../certs/CA/ca.crt")   #Vi litar på klienter med certifikat signerat av CA.
+    #context.verify_mode = ssl.CERT_REQUIRED
+    #context.load_verify_locations("../certs/CA/ca.crt")   #Vi litar på klienter med certifikat signerat av CA.
     context.load_cert_chain("../certs/servers/servers.crt", "../certs/servers/servers.key")
     serving.run_simple("0.0.0.0", 5002, app, use_debugger=True, ssl_context=context)
     #app.run(debug=True, host='0.0.0.0', port='5002')
