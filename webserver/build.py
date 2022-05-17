dfdb652106c72246c3eaf275781ede3dbc011e5a
AhmadGhaemi <ahmad.ghaemi@hotmail.com>
2022-05-17 10:15:45 +0200

    try was wrong

--- webserver/build.py
+++ webserver/build.py
@@ -105,10 +105,11 @@ if __name__ == "build": #"__main__":
         context.load_verify_locations(CA_CRT)   #Vi litar på klienter med certifikat signerat av CA.
         print("Two-way HTTPS is enabled, i.e. client needs to authenticate as well.")
     try:
-        context.load_cert_chain(SVR_CERT, SVR_KEY)
+        context.load_cert_chain(SVR_CRT, SVR_KEY)
         print("Cert and key loaded into context.")
     except:
-        sys.exit("Error starting flask server, idk what we've done")
+        print("Cert and key were not loaded into context for some reason.")
+        #sys.exit("Error starting flask server, idk what we've done")
     #serving.run_simple("0.0.0.0", 5000, app, ssl_context=context)
     print("App is about to run. Hold on")
     app.run(debug=True, host='0.0.0.0', port='5000', ssl_context=context)
